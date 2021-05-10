6dda880cec5c956e9973201bf8e3159678d0fc2c
Abhishek Jain <abhijain@Abhisheks-MacBook-Pro.local>
2021-05-10 09:32:28 +0530

    Solved the median of 2 sorted arrays problem

---
+++ 004_Median_Of_2_Sorted_Arrays/medianOf2SortedArrays.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> res;
+        
+        int i=0,j=0;
+        
+        while(i<nums1.size() && j<nums2.size()){
+            if(nums1[i]<=nums2[j]){
+                res.push_back(nums1[i]);
+                i++;
+            }else{
+                res.push_back(nums2[j]);
+                j++;
+            }
+        }
+        
+        while(i<nums1.size()){
+            res.push_back(nums1[i]);
+            i++;
+        }
+        
+        while(j<nums2.size()){
+            res.push_back(nums2[j]);
+            j++;
+        }
+        
+        
+        int n = nums1.size() + nums2.size();
+        
+        if( n %2  == 0){
+            return (res[n/2] + res[n/2 - 1])/2.0;
+        }
+        else{
+            return res[n/2];
+        }
+        
+    }
+};
