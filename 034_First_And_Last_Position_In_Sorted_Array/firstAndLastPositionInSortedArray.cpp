6ab994ab336d1dfd07790042f93e29fa74f4609c
Abhishek Jain <abhijain@Abhisheks-MacBook-Pro.local>
2021-05-16 09:02:23 +0530

    Solved the first and last position in sorted array problem with another approach

--- 034_First_And_Last_Position_In_Sorted_Array/firstAndLastPositionInSortedArray.cpp
+++ 034_First_And_Last_Position_In_Sorted_Array/firstAndLastPositionInSortedArray.cpp
@@ -53,3 +53,47 @@ public:
         return res;
     }
 };
+
+
+// In place recursion 
+class Solution {
+public:    
+    vector<int> searchRange(vector<int>& nums, int target) {
+       int n = nums.size();
+       int lo = 0;
+        int hi = n-1;
+        
+        vector<int> res(2, -1);
+        
+        while(lo<=hi){
+            int mid = (lo + hi)/2;
+            if(nums[mid] == target){
+                res[0] = mid;
+                hi = mid-1;
+            }else if(nums[mid] < target){
+                lo = mid +1;
+            }
+            else{
+                hi = mid-1;
+            }
+        }
+        
+        lo = 0;
+        hi = n-1;
+        
+        while(lo<=hi){
+            int mid = (lo + hi)/2;
+            if(nums[mid] == target){
+                res[1] = mid;
+                lo = mid+1;
+            }
+            else if(nums[mid] < target){
+                lo = mid+1;
+            }else{
+                hi = mid -1;
+            }
+        }
+        
+        return res;
+    }
+};
